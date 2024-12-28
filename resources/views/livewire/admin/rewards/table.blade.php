7b7b440a18656bdcab4e0341f331fc596205c43b
mbsoft31 <bekhouche.mouadh@gmail.com>
2024-12-28 17:21:27 +0100

    testing badges

--- resources/views/livewire/admin/rewards/table.blade.php
+++ resources/views/livewire/admin/rewards/table.blade.php
@@ -5,23 +5,17 @@ use Illuminate\Support\Facades\Session;
 
 use function Livewire\Volt\{form, layout, state};
 /** @var Array<\Mbsoft\Rewards\DTO\Badge> $badges*/
-state(['badges' => [
-    new \App\Rewards\Badges\FirstPurchaseBadge(),
-    new \App\Rewards\Badges\FrequentShopperBadge(),
-    new \App\Rewards\Badges\MonthlySpenderBadge(),
-]]);
+state(['badges' => []]);
 
-form(LoginForm::class);
-
-$login = function () {
-    $this->validate();
-
-    $this->form->authenticate();
-
-    Session::regenerate();
-
-    $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
+$mount = function () {
+    $this->badges = collect([
+        new \App\Rewards\Badges\FirstPurchaseBadge(),
+        new \App\Rewards\Badges\FrequentShopperBadge(),
+        new \App\Rewards\Badges\MonthlySpenderBadge(),
+    ]);
 };
+
+$mount();
 
 ?>
 
