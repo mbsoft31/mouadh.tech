9de050b98926b6cac5303f0b17bbd06df8652d9d
mbsoft31 <bekhouche.mouadh@gmail.com>
2024-12-31 20:46:27 +0100

    wip!

--- resources/views/livewire/admin/rewards/table.blade.php
+++ resources/views/livewire/admin/rewards/table.blade.php
@@ -4,23 +4,30 @@ use App\Livewire\Forms\LoginForm;
 use Illuminate\Support\Facades\Session;
 
 use function Livewire\Volt\{form, layout, state};
-/** @var Array<\Mbsoft\Rewards\DTO\Badge> $badges*/
-state(['badges' => []]);
 
-$this->badges = collect([
-    new \App\Rewards\Badges\FirstPurchaseBadge(),
-    new \App\Rewards\Badges\FrequentShopperBadge(),
-    new \App\Rewards\Badges\MonthlySpenderBadge(),
+state([
+    'badges' => collect([
+        new \App\Rewards\Badges\FirstPurchaseBadge(),
+        new \App\Rewards\Badges\FrequentShopperBadge(),
+        new \App\Rewards\Badges\MonthlySpenderBadge(),
+    ])->map(function($item) {
+        $item = $item->toArray();
+        $item['criteria'] = collect($item['criteria'])->map(fn($i) => $i->toArray())->toArray();
+        return $item;
+    })->toArray()
 ]);
+
+
 
 ?>
 
 <div>
 
     <div class="flex space-y-10">
+
         @foreach($badges as $badge)
             <div>
-                {{ $badge->name }}
+                {{ $badge['name'] }}
             </div>
         @endforeach
     </div>
